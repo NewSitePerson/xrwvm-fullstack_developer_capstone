f84aff87ea4a66db3ac390ee7df14f6d4a61acaa
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-10-28 12:53:29 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -44,10 +44,8 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-   def logout_request(request):
-    # ...
     logout(request)
-    data = {"userName": ""}
+    data = {"userName":""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
