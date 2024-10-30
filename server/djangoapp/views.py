181b3587e57ce3924b500aa7dec94ddbc7f25981
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-10-30 17:54:58 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -115,8 +115,8 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return HttpResponse(get_request(endpoint))
-    #return JsonResponse({"status": 200, "dealers": dealerships})
+    #return HttpResponse(get_request(endpoint))
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
