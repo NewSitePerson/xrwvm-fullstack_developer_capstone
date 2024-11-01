25112003a91af783045a5f18c20fd01e9f1b6c03
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-10-31 20:50:47 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-ccow$tz_'
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'https://dyharwardia-8000.theiadockernext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://dyharwardia-8000.theiandockernext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['http://localhost',    'http://127.0.0.1',  'https://dyharwardia-8000.theiandockernext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
