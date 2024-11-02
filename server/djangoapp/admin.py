c30d60b5d40b12b843dd94843375045ef982152e
NewSitePerson <160441389+NewSitePerson@users.noreply.github.com>
2024-11-01 22:23:19 -0400

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,7 +5,3 @@ from .models import CarMake, CarModel
 
 admin.site.register(CarMake)
 admin.site.register(CarModel)
- # CarModelInline class
- # CarModelAdmin class
- # CarMakeAdmin class with CarModelInline
- # Register models here
